7b0f13d7edb45a9b81b825a0d97e753794f917e0
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-05 21:30:28 +0500

    Начал создавать базовую сцену

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset = new Vector3(0, 0, -7);
+    public Vector3 offset = new Vector3(0, 0, -10);
 
     void LateUpdate()
     {
