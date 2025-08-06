2412de0674d4ea49f6d9d067c3a88f36eab400de
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-06 13:18:10 +0500

    Добавил здоровье игроку

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+
+    private int MAX_HEALTH = 100;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Damage(10);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Heal(10);
+        }
+    }
+
+    public void Damage(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        this.health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Healing");
+        }
+
+        bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+        if (wouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log($"The {this} is dead");
+        Destroy(gameObject);
+    }
+}
