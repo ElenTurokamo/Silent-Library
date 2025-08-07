b96748a599a491a63aafbdde7406ea673ee14fce
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-07 13:00:33 +0500

    Добавил анимацию отнятия ХП для полоски здоровья. Исправил цвета спрайтов

---
+++ Assets/Scripts/HealthSlidebarUI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlidebarUI : MonoBehaviour
+{
+    public Image frontBar;
+    public Image backBar;
+
+    public Health healthScript;
+    public float lerpSpeed = 2f;
+
+    private float targetFill;
+
+    void Start()
+    {
+        targetFill = 1f;
+        frontBar.fillAmount = 1f;
+        backBar.fillAmount = 1f;
+
+    }
+    void Update()
+    {
+        float current = healthScript.CurrentHealth / healthScript.MaxHealth;
+
+        frontBar.fillAmount = current;
+
+        if (backBar.fillAmount > current)
+        {
+            backBar.fillAmount = Mathf.Lerp(backBar.fillAmount, current, Time.deltaTime * lerpSpeed);
+        }
+        else
+        {
+            backBar.fillAmount = current;
+        }
+    }
+}
