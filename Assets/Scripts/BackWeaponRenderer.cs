811b469c325c391fecd4d7b38c049ae14f59ba13
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-10 00:39:59 +0500

    Разефакторил код, сделал так, чтобы меч после атаки плавно появлялся за спиной

--- Assets/Scripts/BackWeaponRenderer.cs
+++ Assets/Scripts/BackWeaponRenderer.cs
@@ -9,12 +9,18 @@ public class BackWeaponRenderer : MonoBehaviour
     // Слот "оружия"
     public GameObject weaponSprite;
 
+    // Настраиваемые параметры
+    [Header("Parameters")]
+    [SerializeField] private float fadeSpeed = 9f;
+
+    // Положение меча относительно игрока
     [Header("Positions")]
     public Vector3 positionUp;
     public Vector3 positionDown;
     public Vector3 positionLeft;
     public Vector3 positionRight;
 
+    // Угол меча относительно игрока
     [Header("Angles")]
     public Quaternion angleUp;
     public Quaternion angleDown;
@@ -22,6 +28,7 @@ public class BackWeaponRenderer : MonoBehaviour
     public Quaternion angleRight;
 
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     // Метод Start, вызывается один раз при появлении объекта в иерархии
     // Если есть персонаж игрока, то получает его аниматор
@@ -30,22 +37,28 @@ public class BackWeaponRenderer : MonoBehaviour
         if (Character != null)
         {
             animator = Character.GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
     }
 
     // Метод Update, вызывается каждый кадр
-    // 
+    // На основе состояния аниматора игрока убирает меч из-за спины, а также меняет его положение
+    // в пространстве в зависимости от положения игрока.
     void Update()
     {
         if (animator == null || weaponSprite == null) return;
         
         if (animator.GetBool("IsAttacking"))
         {
-            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            Color c = spriteRenderer.color;
+            c.a = 0f;
+            spriteRenderer.color = c;
         }
         else
         {
-            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            Color c = spriteRenderer.color;
+            c.a = Mathf.MoveTowards(c.a, 1f, fadeSpeed * Time.deltaTime);
+            spriteRenderer.color = c;
         }
 
         float moveX = animator.GetFloat("X");
