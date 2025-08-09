c25450565d56c3bf41e83b89f54fe72416c764f2
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-09 23:38:49 +0500

    Рефакторинг кода

--- Assets/Scripts/SyncHandAnimator.cs
+++ Assets/Scripts/SyncHandAnimator.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 
+ // Класс, отвечающий за синхронизацию аниматора рук на основе тела игрока.
 public class SyncHandAnimator : MonoBehaviour
 {
+    // Аниматор тела
     public Animator sourceAnimator;
+    // Аниматор рук
     private Animator myAnimator;
 
+    // Метод Start, вызывается при появлении объекта в иерархии
+    // Получает аниматор прикреплённой руки.
     void Start()
     {
         myAnimator = GetComponent<Animator>();
     }
 
+    // Метод Update, вызывается каждый кадр
+    // Получает состояние аниматора тела и переводит аниматоры рук в то же состояние
     void Update()
     {
         var state = sourceAnimator.GetCurrentAnimatorStateInfo(0);
