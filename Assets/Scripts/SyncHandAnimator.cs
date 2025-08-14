c64c48b30046cf123b1b6e100f00905a267f1004
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-14 14:41:18 +0500

    Переписал скрипт по привязке рук

--- Assets/Scripts/SyncHandAnimator.cs
+++ Assets/Scripts/SyncHandAnimator.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 
- // Класс, отвечающий за синхронизацию аниматора рук на основе тела игрока.
+// Класс, отвечающий за синхронизацию аниматора рук на основе тела игрока.
 public class SyncHandAnimator : MonoBehaviour
 {
-    // Аниматор тела
-    public Animator sourceAnimator;
-    // Аниматор рук
-    private Animator myAnimator;
+    public Animator sourceAnimator;     // Аниматор тела
+    private Animator myAnimator;    // Аниматор рук
+    private bool isDetached = false;    // Изначально аниматор рук всегда привязан к телу
+    private Transform handTransform;
 
     // Метод Start, вызывается при появлении объекта в иерархии
     // Получает аниматор прикреплённой руки.
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+        handTransform = transform;
     }
 
     // Метод Update, вызывается каждый кадр
     // Получает состояние аниматора тела и переводит аниматоры рук в то же состояние
     void Update()
     {
-        var state = sourceAnimator.GetCurrentAnimatorStateInfo(0);
-        myAnimator.Play(state.fullPathHash, 0, state.normalizedTime);
+        if (!isDetached)
+        {
+            var state = sourceAnimator.GetCurrentAnimatorStateInfo(0);
+            myAnimator.Play(state.fullPathHash, 0, state.normalizedTime);
+        }
+    }
+
+    public void DetachAndAim(Vector2 targetPosition)
+    {
+        isDetached = true;
+        Vector2 direction = (targetPosition - (Vector2)handTransform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        handTransform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    public void ReAttach()
+    {
+        isDetached = false;
     }
 }
