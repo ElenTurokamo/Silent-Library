b823140d3159c3d8fcb07059e56f64510aeefdaf
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-14 15:04:26 +0500

    Переписал активацию атаки. Теперь атака будет производиться в зависимости от направления курсора/джойстика игрока

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.Timeline;
 
@@ -11,6 +11,7 @@ public class PlayerAttack : MonoBehaviour
     private float timeToAttack = 0.25f;
     private float timer = 0f;
     private GameObject attackArea = default;
+    private SyncHandAnimator syncHandAnimator;
 
     // Метод, который вызывается при запуске сцены. Передаёт в переменную movement скрипт, отвечающий за движение игрока.
     private void Start()
@@ -49,14 +50,36 @@ public class PlayerAttack : MonoBehaviour
     // Метод атаки игрока. При нажатии левой кнопки мыши игрок атакует в зависимости от направления своего взгляда.
     public void TriggerAttack()
     {
-        Vector2 lookDir = movement.LastLookDir;
+        Vector2 direction;
+        Vector2 cursorWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 cursorDir = (cursorWorldPos - (Vector2)transform.position).normalized;
+
+        if (cursorDir.magnitude > 0.01f)
+        {
+            direction = cursorDir;
+            syncHandAnimator.DetachAndAim(cursorWorldPos);
+
+            anim.SetFloat("X", direction.x);
+            anim.SetFloat("Y", direction.y);
+        }
+        else
+        {
+            direction = movement.LastLookDir;
+
+            anim.SetFloat("X", direction.x);
+            anim.SetFloat("Y", direction.y);
+        }
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (angle > -45 && angle <= 45) anim.SetTrigger("AttackRight");
+        else if (angle > 45 && angle <= 135) anim.SetTrigger("AttackUp");
+        else if (angle < -45 && angle >= -135) anim.SetTrigger("AttackDown");
+        else anim.SetTrigger("AttackLeft");
 
         IsAttacking = true;
-        attackArea.SetActive(IsAttacking);
+        attackArea.SetActive(true);
         anim.SetBool("IsAttacking", true);
-        anim.SetTrigger("TriggerAttack");
-        anim.SetFloat("X", lookDir.x);
-        anim.SetFloat("Y", lookDir.y);
     }
 
     // Вызывается ивентом в анимации, чтобы остановить атаку игрока
