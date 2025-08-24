dbc61a20421f49ff4e97269f1e34502faebfa065
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-08-25 00:13:36 +0500

    Добавил область, которая скрыта пока игрок не пройдёт определенную точку

--- Assets/Scripts/TileTypeFadeToggle.cs
+++ Assets/Scripts/TileTypeFadeToggle.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Tilemaps;
 
-public class TileTypeFadeToggle : MonoBehaviour
+[RequireComponent(typeof(Collider2D))]
+public class TilemapFade : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TilemapRenderer tilemapRenderer; 
+    [SerializeField] private float fadeDuration = 1f;
+
+    private bool isVisible = true;
+    private Coroutine fadeRoutine;
+
+    private void Reset()
     {
-        
+        var col = GetComponent<Collider2D>();
+        if (col) col.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // можно заменить проверку хоть на компонент PlayerMovement
+        if (other.CompareTag("Player"))
+            Toggle();
+    }
+
+    private void Toggle()
+    {
+        if (fadeRoutine != null) StopCoroutine(fadeRoutine);
+        fadeRoutine = StartCoroutine(Fade(!isVisible));
+    }
+
+    private IEnumerator Fade(bool makeVisible)
+    {
+        Color startColor = tilemapRenderer.material.color;
+        Color targetColor = startColor;
+        targetColor.a = makeVisible ? 1f : 0f;
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / fadeDuration;
+            tilemapRenderer.material.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+
+        tilemapRenderer.material.color = targetColor;
+        isVisible = makeVisible;
+        fadeRoutine = null;
     }
 }
